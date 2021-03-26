166c1c9092e15ff4849c2108b745f287f9e1243a
DEV_YKJ <rudwns7552@gmail.com>
2021-03-26 23:35:26 +0900

    * 도메인 클래스 컨버터   - key를 파라미터로 넘기면 자동으로 엔터티로 변환 해주는 기능   - 주의점 :     도메인 클래스 컨버터로 엔티티를 파라미터로 받으면, 이 엔티티는 단순 조회용으로만 사용해야 한다.     (트랜잭션이 없는 범위에서 엔티티를 조회했으므로, 엔티티를 변경해도 DB에 반영되지 않는다.)

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package study.datajpa.controller;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMembers(@PathVariable("id") Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMembers2(@PathVariable("id") Member member){
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    private void init(){
+        Member member = new Member("member1",10);
+        memberRepository.save(member);
+    }
+}
